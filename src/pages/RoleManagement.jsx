19430c8c77bf3fec98fca68e55826ee1e2a0e24f
Purna-Ajay <purnaajay.in@bluecloudsoftech.com>
2026-02-20 01:08:33 +0530

    12

--- src/pages/RoleManagement.jsx
+++ src/pages/RoleManagement.jsx
@@ -399,7 +399,7 @@ export default function RoleManagement() {
     const fetchActiveOrg = async () => {
         try {
             const data = await organizationService.getOrganizations();
-            const active = data.organizations?.find(org => org.is_active);
+            const active = data.organizations?.find(org => org.current);
             if (active) {
                 setActiveOrg(active);
                 fetchRoles(active.slug);
